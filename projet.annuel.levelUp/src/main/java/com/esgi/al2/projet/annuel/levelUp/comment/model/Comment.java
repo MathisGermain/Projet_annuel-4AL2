a2b27660ecd74fdd458e0d44689d52fbc92908a8
f.nicolas95140@gmail.com <f.nicolas95140@gmail.com>
2021-07-22 02:56:18 +0200

    "auth/token/{id}"

--- projet.annuel.levelUp/src/main/java/com/esgi/al2/projet/annuel/levelUp/comment/model/Comment.java
+++ projet.annuel.levelUp/src/main/java/com/esgi/al2/projet/annuel/levelUp/comment/model/Comment.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "comment")
+@Entity(name = "comments")
 public class Comment {
 
         @SequenceGenerator(
--- projet.annuel.levelUp/src/main/java/com/esgi/al2/projet/annuel/levelUp/user/controller/AuthControllerImpl.java
+++ projet.annuel.levelUp/src/main/java/com/esgi/al2/projet/annuel/levelUp/user/controller/AuthControllerImpl.java
@@ -3,10 +3,13 @@ package com.esgi.al2.projet.annuel.levelUp.user.controller;
 import com.esgi.al2.projet.annuel.levelUp.user.model.Login;
 import com.esgi.al2.projet.annuel.levelUp.user.model.User;
 import com.esgi.al2.projet.annuel.levelUp.user.service.AuthService;
+import com.esgi.al2.projet.annuel.levelUp.user.service.SessionService;
+import com.esgi.al2.projet.annuel.levelUp.user.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class AuthControllerImpl implements AuthController{
     private final AuthService authService;
+    private final SessionService sessionService;
+    private final UserService userService;
 
     @Override
     public ResponseEntity<Object> create(@RequestBody User user) {
         var uri = authService.registerUser(user);
         return new ResponseEntity<>(ResponseEntity.created(uri).build(), HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<String> getToken(Integer id){
+        var user = userService.findById(id).get();
+        var s = sessionService.findById(user);
+        var token = s.get().getToken();
+        return new ResponseEntity<>(token, HttpStatus.OK);
     }
 
     @Override
